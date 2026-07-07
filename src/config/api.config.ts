77303ec4e73ac607d7fb262e110a814c9445171d
Sarkis Sanoyan <sarkissanoyan.js@gmail.com>
2026-07-07 18:25:20 +0400

    base-url fix

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,8 +1,8 @@
 const isDev = import.meta.env.DEV;
 
 export const BASE_URL = isDev
-    ? "http://localhost:8080/api/v1"
-    : (import.meta.env.VITE_API_URL || "english-test-generator-backend-production.up.railway.app/api/v1");
+  ? "http://localhost:8080/api/v1"
+  : (import.meta.env.VITE_API_URL || "https://english-test-generator-backend-production.up.railway.app/api/v1");
 
 export const AUTH_API = `${BASE_URL}/auth`;
 export const QUIZ_API = `${BASE_URL}/quiz`;
