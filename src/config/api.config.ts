87b5f4ea7c55f90e017fdd7f0ad3f817e129fa85
Sarkis Sanoyan <sarkissanoyan.js@gmail.com>
2026-07-10 19:26:49 +0400

    results

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -8,6 +8,5 @@ export const AUTH_API = `${BASE_URL}/auth`;
 export const QUIZ_API = `${BASE_URL}/quiz`;
 export const ADMIN_QUIZZES_API = `${BASE_URL}/quiz`;
 export const ADMIN_RESULTS_API = `${BASE_URL}/admin/results`;
-export const USER_RESULTS_API = `${BASE_URL}/results`;
 export const RESULTS_API = ADMIN_RESULTS_API;
 export const USERS_API = `${BASE_URL}/admin/users`;
