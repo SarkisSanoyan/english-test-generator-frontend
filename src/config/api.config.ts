4375988641fa742ee9b0b62ab1b4e8a065f3a7e9
Sarkis Sanoyan <sarkissanoyan.js@gmail.com>
2026-07-10 12:32:32 +0400

    base url change

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -2,7 +2,7 @@ const isDev = import.meta.env.DEV;
 
 export const BASE_URL = isDev
   ? "http://localhost:8080/api/v1"
-  : (import.meta.env.VITE_API_URL || "https://english-test-generator-backend-production.up.railway.app/api/v1");
+  : (import.meta.env.VITE_API_URL || "https://english-test-generator-backend.vercel.app/api/v1");
 
 export const AUTH_API = `${BASE_URL}/auth`;
 export const QUIZ_API = `${BASE_URL}/quiz`;
